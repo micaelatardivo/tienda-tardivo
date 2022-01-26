1bf6c73b29c275040347837252686f56791b67a0
micaelatardivo <micatardivo@gmail.com>
2022-01-26 20:06:26 -0300

    menu e-commerce

---
+++ src/components/Menu.js
@@ -0,0 +1,16 @@
+import './Menu.css';
+
+function Menu() {
+    return (
+        <div className="container">
+            <h1>CUARTO SET</h1>
+                <ul>
+                    <li>Inicio</li>
+                    <li>Productos</li>
+                    <li>Mayorista</li>
+                </ul>
+        </div>
+    );
+}
+
+export default Menu; 
